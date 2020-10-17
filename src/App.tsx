1b209cf23b3f0578c19a7cff2f670f92ace7c0ee
Michael Rocha Leles <rocha_michael@live.com>
2020-10-17 08:49:11 -0300

    Acerto

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,29 @@ function App() {
     if ( !SpeechRecognition.browserSupportsSpeechRecognition() ) {      
       document.getElementById('microfone')?.setAttribute("disabled","disabled");
     }
-  });
+  },[]);
 
   const commands = [
     { command: 'Limpar',
       callback: ({resetTranscript}:any) => resetTranscript()
-    }
+    }    
   ];  
-  const { transcript } = useSpeechRecognition({commands});
+  const { transcript, listening } = useSpeechRecognition({commands});
 
-  const [nome, setNome] = useState('');  
+  const [nome, setNome] = useState('');    
+
+  useEffect(()=>{setNome(transcript)},[transcript]);  
+
+  useEffect(()=>{
+
+    if (!listening) {
+
+      document.getElementById('microfone')!.style.animation = "";
+      document.getElementById('microfone')!.style.borderRadius = "";
+      document.getElementById('microfone')?.classList.remove("pulse");
+      document.getElementById('microfone')?.removeAttribute("disabled");  
+    }
+  },[listening]);
 
   function microfone(){  
 
@@ -29,33 +42,26 @@ function App() {
     document.getElementById('microfone')!.style.borderRadius = "8px";
     document.getElementById('microfone')?.classList.add("pulse");
     document.getElementById('microfone')?.setAttribute("disabled","disabled");
+
+    // setNome("");
   
-    SpeechRecognition.startListening({language: 'pt-BR'});    
+    SpeechRecognition.startListening({language: 'pt-br'});                
+
+    console.log('transcript',transcript);
 
     setTimeout(() => {
       document.getElementById('microfone')!.style.animation = "";
       document.getElementById('microfone')!.style.borderRadius = "";
       document.getElementById('microfone')?.classList.remove("pulse");
-      document.getElementById('microfone')?.removeAttribute("disabled");
-    }, 5000);
-
-          
+      document.getElementById('microfone')?.removeAttribute("disabled");      
+    }, 5500);          
   }
 
   return (
     <div className="App" style={{margin:50}}>
-      {/* <span onChange={(e)=>{setNome(e.currentTarget.innerText)}}>{transcript}</span> <br /> */}
-      <input id="ref" readOnly value={transcript} onChange={(e)=>{console.log(e.target)}}/> <br />
-
-      {/* <input type="text" hidden onChange={(e)=>{setNome(e.target.value)}}/>{transcript} <br/> */}
       <label htmlFor="nome">Nome completo</label> <br/>
-      <input type="text" id="nome" name="nome" style={{width:300}}
-        // value={(nome === '')? transcript: nome}
-        value={nome}
-        onChange={(e)=>{setNome(e.target.value)}}/>
-      <button id="microfone" style={{width:80, height:50, marginLeft:5, borderRadius:5}}         
-        onClick={microfone} >Microfone</button>
-      {/* <p>{transcript}</p> */}
+      <input type="text" id="nome" name="nome" style={{width:300}} value={nome} onChange={(e)=>{setNome(e.target.value)}}/>        
+      <button id="microfone" style={{width:80, height:50, marginLeft:5, borderRadius:5}} onClick={microfone} >Microfone</button>
     </div>
   );
 }
